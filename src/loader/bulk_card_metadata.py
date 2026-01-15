3db5277ad33d4a564e499b0e780f0acb9b2deeb2
Alexander Hristov <alex-hristov@hotmail.com>
2026-01-14 23:25:17 -0500

    start of bulk indexing card metadata

---
+++ src/loader/bulk_card_metadata.py
@@ -0,0 +1,10 @@
+from common.database.elasticsearch import bulk_upsert, es
+from common.database.postgres import get_db_session
+from common.models.postgres_models import CardMetadata
+from common.utils.converter import model_to_dict
+
+if __name__ == "__main__":
+    with get_db_session() as session:
+        for row in session.query(CardMetadata).yield_per(100):
+            print(model_to_dict(row))
+            exit()
